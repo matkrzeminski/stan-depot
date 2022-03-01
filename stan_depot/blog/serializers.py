6a2f4550cc99ed30407b7fc2167fd01874a3f73b
Mateusz Krzeminski <mateuszkrzeminski16@gmail.com>
2022-03-01 21:36:53 +0100

    Start working on blog list template with vuejs.

--- stan_depot/blog/serializers.py
+++ stan_depot/blog/serializers.py
@@ -11,6 +11,7 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
         fields = [
             "id",
             "title",
+            "slug",
             "hero",
             "thumbnail",
             "content",
---
+++ stan_depot/static/js/blog/index.js
@@ -0,0 +1,34 @@
+let app = new Vue({
+  el: '#app',
+  delimiters: ['[[', ']]'],
+  data: {
+    posts: [],
+    pageSize: 1,
+  },
+  methods: {
+    async getPosts(pageSize) {
+      await fetch(`http://127.0.0.1:8000/blog/api/posts/?format=json&page_size=${pageSize}`)
+        .then(response => response.json())
+        .then(data => {
+          for (const result of data['results']) {
+            result['url'] = `${document.URL}${result['slug']}/`;
+            this.posts.push(result);
+          }
+          this.pageSize += 1;
+        }).catch(() => console.error('No more posts'));
+
+    },
+    async scrollGetPosts() {
+      let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+      if (bottomOfWindow) {
+        await this.getPosts(this.pageSize);
+      }
+    }
+  },
+  async beforeMount() {
+    await this.getPosts(this.pageSize);
+  },
+  async mounted() {
+    window.addEventListener('scroll', await this.scrollGetPosts);
+  }
+});
